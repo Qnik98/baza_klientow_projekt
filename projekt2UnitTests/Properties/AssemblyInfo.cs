4bba113401cdabcbc6f972a00fbd76494a173759
Qnik98 <67662329+Qnik98@users.noreply.github.com>
2020-10-11 19:07:27 +0200

    pierwszy commit projektu

---
+++ projekt2UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("projekt2UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("projekt2UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37ec6de7-2ab8-4f56-8122-9f0a4173f748")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ projekt2UnitTests/UnitTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+using NUnit.Framework;
+using projekt2;
+
+namespace projekt2UnitTests
+{
+    
+    [TestFixture]
+    public class UnitTests
+    {
+        [TestCase, Category("Bazowe")]
+        public void Button_Click()
+        {
+            bool res = false;
+            try
+            {
+                SqlConnection conn = new SqlConnection(Globals.ConnString);
+                conn.Open();
+                res = true;
+            }
+            catch (Exception ex)
+            {
+                res = false;
+            }
+            Assert.IsTrue(res);
+            
+        }
+    
+       
+
+        [TestCase, Category("Window 3")]
+        public void Usun_Click()
+        {
+            bool res = false;
+            try
+            {
+                SqlConnection conn = new SqlConnection(Globals.ConnString);
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    SqlCommand createCommand = new SqlCommand("SELECT COUNT(0) FROM KLIENT_DANE", conn);
+                    int maxNr = (int)createCommand.ExecuteScalar() - 1;
+
+                    String query = $"DELETE FROM KLIENT_DANE WHERE ID = '" + maxNr + "'";
+                    createCommand.CommandText = query;
+                    int rows = createCommand.ExecuteNonQuery();
+
+                    res = rows > 0;
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                res = false;
+            }
+            Assert.IsTrue(res);
+        }
+    }
+}
